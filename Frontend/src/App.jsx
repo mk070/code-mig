86107721d1e8e60588d221f8aef2d45445979a50
santhosh24082003 <chendrayasanthosh05@gmail.com>
2024-08-15 13:06:16 +0530

    Worked on the functionalities of the run-button

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,14 +2,17 @@ import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage'; 
 import ProductPage from './pages/ProductPage';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/product" element={<ProductPage/>} />
+        <Route path="/product" element={<ProductPage />} />
       </Routes>
+      <ToastContainer />
     </Router>
   );
 }
--- Frontend/src/Components/UploadButton.jsx
+++ Frontend/src/Components/UploadButton.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import UploadIcon from '@mui/icons-material/Upload';
 import FileUploadPopup from './FileUploadPopup';
 
-const UploadButton = () => {
+const UploadButton = ({ onFileUpload, onGithubLinkChange }) => {
   const [openPopup, setOpenPopup] = useState(false);
 
   const handleButtonClick = () => {
@@ -13,12 +13,18 @@ const UploadButton = () => {
     <>
       <button
         onClick={handleButtonClick}
-        className="ml-2 w-full p-2.5 bg-blue-600  text-white text-sm font-medium rounded-md shadow-sm flex items-center justify-center"
+        className="ml-2 w-full p-2.5 bg-blue-600 text-white text-sm font-medium rounded-md shadow-sm flex items-center justify-center"
       >
         <UploadIcon className="mr-2" />
         Upload Files or Link
       </button>
-      {openPopup && <FileUploadPopup onClose={() => setOpenPopup(false)} />}
+      {openPopup && (
+        <FileUploadPopup
+          onClose={() => setOpenPopup(false)}
+          onFileUpload={onFileUpload}
+          onGithubLinkChange={onGithubLinkChange}
+        />
+      )}
     </>
   );
 };
